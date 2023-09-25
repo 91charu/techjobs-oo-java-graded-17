21b899b9cf81aa5d73a6040d0df7d485708e56e3
Charu <charu91.sathish@gmail.com>
2023-09-25 12:05:56 -0500

    passed task 2 in assign 2

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -35,4 +37,20 @@ public class PositionType {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return  value ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType that)) return false;
+        return getId() == that.getId() && Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue());
+    }
 }
